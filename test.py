01567d3405cab3b86f67b97a8f25839b1f73c5a7
IAmOZRules <shreyaansnahata2001@gmail.com>
2021-11-22 21:49:04 +0530

    added accuracy report

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 import json
+
+from nltk.util import pr
 from chat import get_response
 from prettytable import PrettyTable
 
 with open('json/test.json') as json_file:
     data = json.load(json_file)
 
+count = 0
 t = PrettyTable(["Question", "Actual Tag", "Predicted Tag", "Match Tag"])
 
 for question in data:
     _, tag = get_response(question)
     t.add_row([question, data[question], tag, tag == data[question]])
+
+    if tag == data[question]:
+        count += 1
+
+print(f"\nAccuracy: {count/50 * 100}%\n")
 
 print(t)
