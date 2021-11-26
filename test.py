b16ef528ff43bd54cddcb26282d8b872005a4baf
IAmOZRules <shreyaansnahata2001@gmail.com>
2021-11-26 11:23:51 +0530

    fixed test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ count = 0
 t = PrettyTable(["Question", "Actual Tag", "Predicted Tag", "Match Tag"])
 
 for question in data:
-    _, tag = get_response(question)
+    _, tag, _ = get_response(question)
     t.add_row([question, data[question], tag, tag == data[question]])
 
     if tag == data[question]:
